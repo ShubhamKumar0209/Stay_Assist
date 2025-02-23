6a63265e9261c3ce6a57177feca81047021e325b
shreyathinks <157294155+shreyathinks@users.noreply.github.com>
2025-02-23 18:35:20 +0000

    added new files

--- stay assist/website/views.py
+++ stay assist/website/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 views=Blueprint('views',__name__)
 @views.route('/')
 def home():
-    return "<h1>Website is working<h1>"
+    return render_template("landing.html",boolean=True)
